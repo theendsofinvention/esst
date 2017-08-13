3cdf8dd66447a74c3027b09959ed0cd6e6fe296a
132nd-etcher <132nd-etcher@daribouca.net>
2017-08-13 19:17:25 +0200

    fixed __main__ not catching KeyboardInterrupt

--- esst/__main__.py
+++ esst/__main__.py
@@ -26,64 +26,65 @@ def main(bot: bool, server: bool, socket: bool):
     from esst.core.logger import MAIN_LOGGER
     from esst.core.version import __version__
 
-    MAIN_LOGGER.debug(f'starting ESST {__version__}')
+    try:
+        MAIN_LOGGER.debug(f'starting ESST {__version__}')
 
-    from esst.dcs import game_gui
-    game_gui.install_game_gui_hooks()
+        from esst.dcs import game_gui
+        game_gui.install_game_gui_hooks()
 
-    if bot:
-        from esst.discord_bot import DiscordBot
-        MAIN_LOGGER.debug('starting Discord bot')
-        DiscordBot()
-        blinker.signal('discord message').send(__name__, msg='Hello!')
+        if bot:
+            from esst.discord_bot import DiscordBot
+            MAIN_LOGGER.debug('starting Discord bot')
+            DiscordBot()
+            blinker.signal('discord message').send(__name__, msg='Hello!\nType "!help" for a list of available commands')
 
-    if server:
-        from esst import dcs
-        MAIN_LOGGER.debug('starting DCS monitoring')
-        app = dcs.App()
-        app.start()
+        if server:
+            from esst import dcs
+            MAIN_LOGGER.debug('starting DCS monitoring')
+            app = dcs.App()
+            app.start()
 
-    if socket:
-        from esst import dcs
-        MAIN_LOGGER.debug('starting socket')
-        dcs.DCSListener()
+        if socket:
+            from esst import dcs
+            MAIN_LOGGER.debug('starting socket')
+            dcs.DCSListener()
 
-    while True:
-        try:
+        while True:
             time.sleep(0.1)
-        except KeyboardInterrupt:
 
-            MAIN_LOGGER.info('ESST has been interrupted by user request, closing all threads')
+    except KeyboardInterrupt:
 
-            def _exit_gracefully(thread_name):
-                ready_to_exit = False
+        MAIN_LOGGER.info('ESST has been interrupted by user request, closing all threads')
 
-                def _ready_to_exit(*_):
-                    nonlocal ready_to_exit
-                    MAIN_LOGGER.debug(f'{thread_name} is ready to exit')
-                    ready_to_exit = True
+        def _exit_gracefully(thread_name):
+            ready_to_exit = False
 
-                blinker.signal(f'{thread_name} ready to exit').connect(_ready_to_exit)
-                blinker.signal(f'{thread_name} command').send(__name__, cmd='exit')
+            def _ready_to_exit(*_):
+                nonlocal ready_to_exit
+                MAIN_LOGGER.debug(f'{thread_name} is ready to exit')
+                ready_to_exit = True
 
-                now = time.time()
-                while not ready_to_exit:
-                    if time.time() > now + 20:
-                        MAIN_LOGGER.error(f'{thread_name} thread did not exit gracefully')
-                        break
-                    time.sleep(0.1)
+            blinker.signal(f'{thread_name} ready to exit').connect(_ready_to_exit)
+            blinker.signal(f'{thread_name} command').send(__name__, cmd='exit')
 
-            if server:
-                _exit_gracefully('dcs')
+            now = time.time()
+            while not ready_to_exit:
+                if time.time() > now + 20:
+                    MAIN_LOGGER.error(f'{thread_name} thread did not exit gracefully')
+                    break
+                time.sleep(0.1)
 
-            if bot:
-                _exit_gracefully('discord')
+        if server:
+            _exit_gracefully('dcs')
 
-            if socket:
-                _exit_gracefully('socket')
+        if bot:
+            _exit_gracefully('discord')
 
-            # noinspection PyProtectedMember
-            os._exit(0)  # pylint: disable=protected-access
+        if socket:
+            _exit_gracefully('socket')
+
+        # noinspection PyProtectedMember
+        os._exit(0)  # pylint: disable=protected-access
 
 
 if __name__ == '__main__':
