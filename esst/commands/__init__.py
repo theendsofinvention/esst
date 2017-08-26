c6fbe76a014330c29e48c4dd0abeed33b8496fe0
132nd-etcher <132nd-etcher@daribouca.net>
2017-08-26 10:00:52 +0200

    new: dev: create commands for DCS and Discord

---
+++ esst/commands/__init__.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+
+from esst.discord_bot.commands import DISCORD
+from esst.listener.commands import LISTENER
+from esst.dcs.commands import DCS
---
+++ esst/dcs/commands.py
@@ -0,0 +1,61 @@
+# coding=utf-8
+
+
+from esst.core import CTX, MAIN_LOGGER
+# from .missions_manager import list_available_missions
+
+LOGGER = MAIN_LOGGER.getChild(__name__)
+
+
+class DCS:
+    """
+    Manages commands for the DCS application
+    """
+
+    @staticmethod
+    def restart():
+        """
+        Sets the context to restart the DCS application
+        """
+        LOGGER.debug('setting context for DCS restart')
+        CTX.dcs_do_restart = True
+
+    @staticmethod
+    def kill():
+        LOGGER.debug('killing DCS application')
+        CTX.dcs_do_kill = True
+
+    @staticmethod
+    def show_cpu_usage_once():
+        LOGGER.debug('show cpu usage once')
+        CTX.dcs_show_cpu_usage_once = True
+
+    @staticmethod
+    def show_cpu_usage_once_done():
+        LOGGER.debug('show cpu usage once: done')
+        CTX.dcs_show_cpu_usage_once = False
+
+    @staticmethod
+    def show_cpu_usage_start():
+        LOGGER.debug('show cpu usage: start')
+        CTX.dcs_show_cpu_usage = True
+
+    @staticmethod
+    def show_cpu_usage_stop():
+        LOGGER.debug('show cpu usage: stop')
+        CTX.dcs_show_cpu_usage = False
+
+    @staticmethod
+    def can_start():
+        LOGGER.debug('DCS can start')
+        CTX.dcs_can_start = True
+
+    @staticmethod
+    def cannot_start():
+        LOGGER.debug('DCS can NOT start')
+        CTX.dcs_can_start = False
+
+    @staticmethod
+    def get_mission_list():
+        return []
+        # yield from list_available_missions()
