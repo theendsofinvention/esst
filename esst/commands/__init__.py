757ffddf3afafaed848c3c2391bf6e2cc0ecb0fb
132nd-etcher <132nd-etcher@daribouca.net>
2017-08-26 16:39:51 +0200

    new: dev: add SERVER commands

--- esst/commands/__init__.py
+++ esst/commands/__init__.py
@@ -3,3 +3,4 @@
 from esst.discord_bot.commands import DISCORD
 from esst.listener.commands import LISTENER
 from esst.dcs.commands import DCS
+from esst.server.commands import SERVER
---
+++ esst/server/commands.py
@@ -0,0 +1,35 @@
+# coding=utf-8
+
+
+import os
+from esst.core import MAIN_LOGGER, CTX
+
+LOGGER = MAIN_LOGGER.getChild(__name__)
+
+
+
+class SERVER:
+
+    @staticmethod
+    def reboot():
+        os.system('shutdown /r /t 30 /c "Shutdown initialized by ESST"')
+
+    @staticmethod
+    def show_cpu_usage_once():
+        LOGGER.debug('show cpu usage once')
+        CTX.server_show_cpu_usage_once = True
+
+    @staticmethod
+    def show_cpu_usage_once_done():
+        LOGGER.debug('show cpu usage once: done')
+        CTX.server_show_cpu_usage_once = False
+
+    @staticmethod
+    def show_cpu_usage_start():
+        LOGGER.debug('show cpu usage: start')
+        CTX.server_show_cpu_usage = True
+
+    @staticmethod
+    def show_cpu_usage_stop():
+        LOGGER.debug('show cpu usage: stop')
+        CTX.server_show_cpu_usage = False
