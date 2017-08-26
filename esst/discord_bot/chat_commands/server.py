d8cd808db892d2e67dd52301936b67d6d82d8378
132nd-etcher <132nd-etcher@daribouca.net>
2017-08-26 16:40:42 +0200

    new: add "!server status" command

--- esst/discord_bot/chat_commands/server.py
+++ esst/discord_bot/chat_commands/server.py
@@ -1,13 +1,28 @@
 # coding=utf-8
 
 import argh
+import humanize
+
+from esst.core import ServerStatus
+from esst.commands import DISCORD, SERVER
 
 
 def status():
     """
     Show current server status
     """
-    print('Server status')
+    output = []
+    for attr_name in dir(ServerStatus):
+        if attr_name.startswith('_'):
+            continue
+        attr_nice_name = (attr_name[:1].upper() + attr_name[1:]).replace("_", " ")
+        value = getattr(ServerStatus, attr_name)
+        if ('memory' in attr_name or 'swap' in attr_name) and value != 'unknown':
+            value = humanize.naturalsize(value)
+        if attr_name in ['cpu_usage']:
+            value = str(value) + '%'
+        output.append(f'{attr_nice_name}: {value}')
+    DISCORD.say('Server status:\n' + '\n'.join(output))
 
 
 @argh.arg('--start', help='Show CPU usage in real time')
