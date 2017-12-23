e8cf6133136cbb3905b4ce25b52cfe38735a82fd
132nd-etcher <132nd-etcher@daribouca.net>
2017-12-23 19:29:33 +0100

    chg: dev: renamed _esst_version -> _version

--- esst/_version.py
+++ esst/_version.py
@@ -43,7 +43,7 @@ def get_config():
     cfg.style = "pep440"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = "None"
-    cfg.versionfile_source = "esst/_esst_version.py"
+    cfg.versionfile_source = "esst/_version.py"
     cfg.verbose = False
     return cfg
 
