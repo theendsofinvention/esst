7b305bce30013bddb9a2380c24731bca0f4ddcfa
132nd-etcher <132nd-etcher@daribouca.net>
2017-12-27 03:32:16 +0100

    chg: dev add base classes to export Sentry context

---
+++ esst/utils/sentry_context.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+"""
+Manages base classes for Sentry contexts
+"""
+import inspect
+
+
+class SentryClassContext:
+    """
+    Base class for data class (class methods only)
+    """
+
+    @classmethod
+    def get_context(cls) -> dict:
+        """
+
+        Returns: dict context for Sentry
+
+        """
+        return {
+            member: value
+            for member, value in inspect.getmembers(cls, lambda a: not inspect.ismethod(a))
+            if not member.startswith('_')
+        }
+
+
+class SentryContext:
+    """
+    Base class for full blown class
+    """
+
+    def get_context(self) -> dict:
+        """
+
+        Returns: dict context for Sentry
+
+        """
+        return {
+            member: value
+            for member, value in inspect.getmembers(self, lambda a: not inspect.ismethod(a))
+            if not member.startswith('_')
+        }
