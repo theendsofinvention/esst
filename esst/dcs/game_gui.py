af1265c6deb041cc3743d8c520c7181d97351231
132nd-etcher <132nd-etcher@daribouca.net>
2018-01-15 21:03:20 +0100

    fix: fix DCS API hook for 1.5.8
    "!dcs status" command will work again, as will the "soft kill" command used to restart DCS

--- esst/dcs/game_gui.py
+++ esst/dcs/game_gui.py
@@ -3,13 +3,14 @@
 Manages GameGUI hooks
 """
 
-import os
+import elib
 
 from esst.core import CTX, FS, MAIN_LOGGER
 from esst.utils import read_template
 
 LOGGER = MAIN_LOGGER.getChild(__name__)
-FILE_PATH = os.path.join(FS.saved_games_path, 'DCS/Scripts/ESSTGameGUI.lua')
+OLD_FILE_PATH = elib.path.ensure_file(FS.saved_games_path, 'DCS/Scripts/ESSTGameGUI.lua', must_exist=False)
+FILE_PATH = elib.path.ensure_file(FS.saved_games_path, 'DCS/Scripts/Hooks/esst.lua', must_exist=False)
 
 
 # noinspection SpellCheckingInspection
@@ -20,7 +21,12 @@ def install_game_gui_hooks():
     Installs the GameGUI hooks in DCS Scripts folder
     """
 
+    if OLD_FILE_PATH.exists():
+        LOGGER.debug('removing old GameGUI script')
+        OLD_FILE_PATH.unlink()
+
     if CTX.dcs_install_hooks:
+        elib.path.ensure_dir(FILE_PATH.parent, must_exist=False, create=True)
         LOGGER.debug('installing GameGUI hooks')
         with open(FILE_PATH, 'w') as stream:
             stream.write(read_template('game_gui.lua'))
