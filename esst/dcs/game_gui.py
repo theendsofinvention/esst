2a4afb2cee11dab39f5f5857dbe4913bbde23493
132nd-etcher <132nd-etcher@daribouca.net>
2018-02-11 16:32:59 +0100

    reduce complexity

--- esst/dcs/game_gui.py
+++ esst/dcs/game_gui.py
@@ -14,23 +14,35 @@ LOGGER = MAIN_LOGGER.getChild(__name__)
 # noinspection SpellCheckingInspection
 
 
-def install_game_gui_hooks():
-    """
-    Installs the GameGUI hooks in DCS Scripts folder
-    """
+def _remove_old_file():
     old_file_path = elib.path.ensure_file(FS.variant_saved_games_path, 'DCS/Scripts/ESSTGameGUI.lua', must_exist=False)
-
-    hook_folder = elib.path.ensure_dir(FS.variant_saved_games_path, 'Scripts/Hooks', must_exist=False, create=True)
-    file_path = elib.path.ensure_file(hook_folder, 'esst.lua', must_exist=False)
 
     if old_file_path.exists():
         LOGGER.debug('removing old GameGUI script')
         old_file_path.unlink()
 
+
+def _install_hook():
+
+    hook_folder = elib.path.ensure_dir(FS.variant_saved_games_path, 'Scripts/Hooks', must_exist=False, create=True)
+    LOGGER.debug(f'hooks folder: {hook_folder}')
+    esst_hook_path = elib.path.ensure_file(hook_folder, 'esst.lua', must_exist=False)
+    LOGGER.debug(f'ESST hook path: {esst_hook_path}')
+    elib.path.ensure_dir(esst_hook_path.parent, must_exist=False, create=True)
+    LOGGER.debug('writing ESST hook to file')
+    with open(esst_hook_path, 'w') as stream:
+        stream.write(read_template('game_gui.lua'))
+
+
+
+def install_game_gui_hooks():
+    """
+    Installs the GameGUI hooks in DCS Scripts folder
+    """
+    _remove_old_file()
+
     if CTX.dcs_install_hooks:
-        elib.path.ensure_dir(file_path.parent, must_exist=False, create=True)
         LOGGER.debug('installing GameGUI hooks')
-        with open(file_path, 'w') as stream:
-            stream.write(read_template('game_gui.lua'))
+        _install_hook()
     else:
         LOGGER.debug('skipping installation of GameGUI hooks')
