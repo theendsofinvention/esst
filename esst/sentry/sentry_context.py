048551bf8edcb2a7643a66ca5fa270fcfe1e9970
etcher <github@daribouca.net>
2018-10-08 22:08:40 +0200

    chg: dev: sanitize Sentry package

--- esst/sentry/sentry_context.py
+++ esst/sentry/sentry_context.py
@@ -24,6 +24,25 @@ class SentryClassContext:
         }
 
 
+class SentryConfigContext:
+    """
+    Base class for data class (class methods only)
+    """
+
+    @classmethod
+    def get_context(cls) -> dict:
+        """
+
+        Returns: dict context for Sentry
+
+        """
+        return {
+            member: value()
+            for member, value in inspect.getmembers(cls, lambda a: not inspect.ismethod(a))
+            if not member.startswith('_')
+        }
+
+
 class SentryContext:
     """
     Base class for full blown class
